7c0f7496dfee637141f3baa10da8bcfc7ff2636a
Rishabh Ranjan <rishabh9050@gmail.com>
2023-10-01 17:59:20 +0530

    Added tracing in A* Algorithm

--- A Star Algorithm.py
+++ A Star Algorithm.py
@@ -1,15 +1,22 @@
-def AstarAlg(startnode,stopnode):
-    openset=set(startnode)
-    closedset=set()
-    g={}
-    parents={}
-    g[startnode]=0
-    parents[startnode]=startnode
+def AstarAlg(startnode, stopnode):
+    openset = set(startnode)
+    closedset = set()
+    g = {}
+    parents = {}
+    g[startnode] = 0
+    parents[startnode] = startnode
+
+    print(f"Startnode: {startnode}")
+    print(f"Stopnode: {stopnode}")
+
     while len(openset) > 0:
         n = None
         for v in openset:
-            if n == None or g[v] + heuristic(v) < g[n] + heuristic(n):
+            if n is None or (g[v] + heuristic(v) < g[n] + heuristic(n)):
                 n = v
+        
+        print(f"Current Node: {n}")
+
         if n == stopnode or Graphnodes[n] is None:
             pass
         else:
@@ -18,16 +25,20 @@ def AstarAlg(startnode,stopnode):
                     openset.add(m)
                     parents[m] = n
                     g[m] = g[n] + weight
+                    print(f"Added {m} to openset with g = {g[m]}")
                 else:
                     if g[m] > g[n] + weight:
                         g[m] = g[n] + weight
                         parents[m] = n
-                    if m in closedset:
-                        closedset.remove(m)
-                        openset.add(m)
+                        if m in closedset:
+                            closedset.remove(m)
+                            openset.add(m)
+                            print(f"Updated {m} in openset with g = {g[m]}")
+
         if n is None:
-            print('path does not exist!')
+            print('Path does not exist!')
             return None
+        
         if n == stopnode:
             path = []
             while parents[n] != n:
@@ -35,13 +46,14 @@ def AstarAlg(startnode,stopnode):
                 n = parents[n]
             path.append(startnode)
             path.reverse()
-            print('path found: {}'.format(path))
+            print(f'\nPath found: {path}')
             return path
+        
         openset.remove(n)
         closedset.add(n)
-    print('path does not exist!')
-    return None
 
+    print('Path does not exist!')
+    return None
 
 def getneighbours(v):
     if v in Graphnodes:
@@ -49,11 +61,9 @@ def getneighbours(v):
     else:
         return None
 
-
 def heuristic(n):
     Hdist = {'S': 5, 'A': 3, 'B': 4, 'C': 2, 'D': 6, 'G': 0 }
     return Hdist[n]
-
 
 Graphnodes = {
     'S': [('A', 1), ('G', 10)], 
@@ -63,4 +73,7 @@ Graphnodes = {
     'D': [('G', 2)]
 }
 
-AstarAlg('S', 'G')
+# Call the A* algorithm with trace
+trace = AstarAlg('S', 'G')
+
+# print(trace)
